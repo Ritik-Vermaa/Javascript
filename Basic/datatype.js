d0ce5f93dda53f2cbf23a4549dde122a0045cbd1
Ritik Verma <vermaritik933@gmail.com>
2023-08-24 12:29:51 +0530

    First commit

---
+++ Basic/datatype.js
@@ -0,0 +1,7 @@
+//alert("Hello") alert only use the web browser show alert the web page
+
+let name = "Ritik"
+let age = 18
+
+
+console.log(typeof name);
---
+++ Basic/variable.js
@@ -0,0 +1,10 @@
+const AccountID = 751844
+let AccountEmail = 'ritik@gmail.com'
+var AccountPassword = "4456"
+let accountCity = "Gola"
+
+
+
+console.log(AccountEmail);
+
+// use let to difine variable 
